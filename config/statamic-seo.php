ca73c59110c24b2f8f44dcbfc602843d45edf62e
Stuart Cusack <stuart@stuartcusack.ie>
2022-07-09 13:07:16 +0100

    Update statamic-seo.php

--- config/statamic-seo.php
+++ config/statamic-seo.php
@@ -6,6 +6,6 @@ return [
     'title' => 'Non-Entry Meta Title',
     'title_append' => '- Entry Append Meta Title',
     'og_image' => null,
-    'excluded_blueprints' => [''],
+    'excluded_blueprints' => [],
 
 ];
