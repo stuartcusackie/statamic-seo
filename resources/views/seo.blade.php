ecdaa57cb90bdc0f8eadcd8d2d9d50fd4420485e
Stuart Cusack <stuart@stuartcusack.ie>
2022-09-06 14:19:19 +0100

    Tighten up template

--- resources/views/seo.blade.php
+++ resources/views/seo.blade.php
@@ -1,24 +1,16 @@
 <title>{{ SEO::metaTitle() }}</title>
 <meta name="description" content="{{ SEO::metaDescription() }}" />
-
 @if($updatedAt = SEO::updatedAt())
 	<meta property="article:modified_time" content="{{ $updatedAt }}" />
 @endif
-
 @stack('canonical')
-
-{{--
-Optional attributes example
-<meta property="article:publisher" content="https://www.facebook.com/YourBrand/" />
---}}
-
 <meta property="og:type" content="website" />
 <meta property="og:locale" content="{{ SEO::locale() }}" />
 <meta property="og:site_name" content="{{ config('statamic-seo.site_name') }}" />
 <meta property="og:url" content="{{ url()->current() }}" />
 <meta property="og:title" content="{{ SEO::ogTitle() }}" />
 <meta property="og:description" content="{{ SEO::ogDescription() }}" />
-
+{{--meta property="article:publisher" content="https://www.facebook.com/YourBrand/" />--}}
 @if($ogImage = SEO::ogImage())
 	@foreach (Statamic::tag('glide:generate')->src($ogImage)->width(1200)->height(627)->fit('crop_focal')->fm('jpg') as $image)
 		<meta property="og:image" content="{!! url($image['url']) !!}" />
